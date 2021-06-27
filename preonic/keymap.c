e26f6106cdd2d194114772ac0b4442d0334f78f7
FailFish <likeinstein42@gmail.com>
2021-06-27 16:03:14 +0900

    minor update from lab

--- preonic/keymap.c
+++ preonic/keymap.c
@@ -47,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	 * |------+------+------+------+------+------|------+------+------+------+------+------|
 	 * |LShift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |RShift|
 	 * |------+------+------+------+------+------+------+------+------+------+------+------|
-	 * |LCtrl |      | GUI  | Alt  |Lower |    Space    |Raise |ko/en | RAlt |      |RCtrl |
+	 * |LCtrl |      | GUI  | Alt  |Lower |Space |LShift|Raise |ko/en | RAlt |      |RCtrl |
 	 * `-----------------------------------------------------------------------------------'
 	 */
 	[_QWERTY] = LAYOUT_preonic_grid(
@@ -55,7 +55,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 			KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_QUOT,
 			KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
 			KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-			KC_LCTL, KC_NO,   KC_LGUI, KC_LALT, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_HAEN, KC_RALT, KC_NO,   KC_RCTL
+			KC_LCTL, KC_NO,   KC_LGUI, KC_LALT, LOWER,   KC_SPC,  KC_LSFT, RAISE,   KC_HAEN, KC_RALT, KC_NO,   KC_RCTL
 			),
 
 	/* Colemak
