386dbc2c69c0d4ed66422c579c57590c5fe2af8d
FailFish <likeinstein42@gmail.com>
2021-09-02 20:06:39 +0900

    bspwm: 'hostname' must be installed w/ 'inetutils'

--- terraform.sh
+++ terraform.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-# sudo pacman -S --needed nvidia-lts reflector pkgfile pacman-contrib xclip zip unzip cmake python python-pip base-devel tmux htop wget curl intel-ucode sudo firefox alacritty zathura-pdf-mupdf tldr tree fish fzf ripgrep fd exa bat procs xorg xorg-xinit bspwm picom sxhkd tint2 rofi feh redshift lesspipe adobe-source-han-sans-kr-fonts adobe-source-han-serif-kr-fonts ttf-fira-code ttf-dejavu bluez bluez-utils openssh nnn dolphin qt5ct breeze-icons lxappearance-gtk3 papirus-icon-theme fcitx5-im fcitx5-hangul
+sudo pacman -S --needed nvidia-lts reflector pkgfile pacman-contrib xclip zip unzip cmake python python-pip base-devel tmux htop wget curl intel-ucode sudo firefox alacritty zathura-pdf-mupdf tldr tree fish fzf ripgrep fd exa bat procs xorg xorg-xinit bspwm picom sxhkd tint2 rofi feh redshift lesspipe adobe-source-han-sans-kr-fonts adobe-source-han-serif-kr-fonts ttf-fira-code ttf-dejavu bluez bluez-utils openssh nnn dolphin qt5ct breeze-icons lxappearance-gtk3 papirus-icon-theme fcitx5-im fcitx5-hangul inetutils
 
 if ! command -v paru &> /dev/null
 then
     echo "paru must be installed first!"
     exit
 fi
-# paru -S nerd-fonts-inter otf-nerd-fonts-monacob-mono nerd-fonts-roboto-mono nerd-fonts-fira-code elementary-planner
+paru -S nerd-fonts-inter otf-nerd-fonts-monacob-mono nerd-fonts-roboto-mono nerd-fonts-fira-code # elementary-planner
 
-# systemctl enable NetworkManager.service bluetooth.service sshd.service
+systemctl enable bluetooth.service
 
 ln -sf $(pwd)/.bashrc $HOME/.bashrc
 ln -sf $(pwd)/.vimrc $HOME/.vimrc
