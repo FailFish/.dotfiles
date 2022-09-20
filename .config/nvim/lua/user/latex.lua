bf3b2a41055f99c17f6dda0eb0116f28713596e7
FailFish <likeinstein42@gmail.com>
2022-09-20 15:34:41 +0900

    nvim; fix vimtex's package doc keymap

--- .config/nvim/lua/user/latex.lua
+++ .config/nvim/lua/user/latex.lua
@@ -1,8 +1,9 @@
 vim.g.vimtex_view_forward_search_on_start = 0
 vim.g.vimtex_view_method = "zathura"
-if not vim.fn.executable("pplatex") then -- error handling
+if vim.fn.executable("pplatex") == 1 then -- error handling
   vim.g.vimtex_quickfix_method = "pplatex"
 end
+vim.keymap.set("n", "<localleader>ld", "<plug>(vimtex-doc-package)", { buffer = true }) -- avoid overriding w/ lsp.hover
 
 vim.g.vimtex_compiler_latexmk = {
   build_dir = 'build',
