c11feb857af4a24ab8e6b0c56a977729930836e3
FailFish <likeinstein42@gmail.com>
2022-11-19 17:21:22 +0900

    nvim; removing packer dependency(WIP)

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,6 @@
 require "user.options"
 require "user.keymaps"
-require "user.plugins"
+-- require "user.plugins"
 require "user.colorscheme"
 require "user.completion"
 require "user.diagnostic"
@@ -14,3 +14,23 @@ require "user.disable_builtin"
 require "user.feline"
 
 require "impatient".enable_profile()
+
+
+if vim.fn.has("macunix") == 1 then
+  require("Comment").setup()
+  require("symbols-outline").setup {}
+  require("lsp_signature").setup {}
+  require("trouble").setup {
+    auto_preview = false,
+    -- auto_fold = true,
+  }
+  require'alpha'.setup(require'alpha.themes.startify'.config)
+  -- require("nnn").setup {
+  --   replace_netrw = "explorer",
+  -- }
+  require("project_nvim").setup {
+    exclude_dirs = { "~/.dotfiles/*" },
+    silent_chdir = false, -- verbose directory changing
+  }
+  require("user.latex")
+end
