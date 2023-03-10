0ef72e9ef18ef79a12a6fabde7a2efcc53abb79d
FailFish <likeinstein42@gmail.com>
2023-03-10 09:55:31 +0900

    nvim; toggleterm(lazygit)

--- .config/nvim/lua/user/terminal.lua
+++ .config/nvim/lua/user/terminal.lua
@@ -32,6 +32,35 @@ toggleterm.setup({
   },
 })
 
+
+-- app-specific terminals
+-- local Terminal  = require('toggleterm.terminal').Terminal
+--
+-- local lazygit = Terminal:new({
+--   cmd = "lazygit",
+--   -- dir = "git_dir",
+--   direction = "float",
+--   float_opts = {
+--     border = "double",
+--   },
+--   -- function to run on opening the terminal
+--   on_open = function(term)
+--     vim.cmd("startinsert!")
+--     -- vim.api.nvim_buf_set_keymap( term.bufnr, "n", "q", "<cmd>close<CR>", { noremap = true, silent = true })
+--     vim.keymap.set("n", "q", "<cmd>close<CR>", { noremap = true, silent = true, buffer = term.bufnr })
+--   end,
+--   hidden = true,
+--   -- function to run on closing the terminal
+--   -- on_close = function(term)
+--   --   vim.cmd("startinsert!")
+--   -- end,
+-- })
+
+-- vim.keymap.set("n", "<leader>G", function()
+--   lazygit:toggle()
+-- end, { noremap = true, silent = true })
+
+-- keybinds
 function _G.set_terminal_keymaps()
   local opts = { buffer = 0 }
   vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
