cbb5519c32db5318cea343cb12bf47195a0df24c
Taehyun Noh <taehyun@utexas.edu>
2026-07-03 15:02:51 -0400

    bitwarden ssh agent

--- users/taehyun-macos.nix
+++ users/taehyun-macos.nix
@@ -5,4 +5,7 @@
 
   home.username = "taehyun";
   home.homeDirectory = "/Users/taehyun";
+  home.sessionVariables = {
+    SSH_AUTH_SOCK="${config.home.homeDirectory}/.bitwarden-ssh-agent.sock";
+  };
 }
